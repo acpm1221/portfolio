fb57125b646783579ff851aa04964369374a09e6
Anmol Kumar <132394800+acpm1221@users.noreply.github.com>
2025-07-16 17:47:06 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,26 @@ const Projects = ({ id }) => {
       <div className="project-list">
         <div className="project-item">
           <h3>Money-Mate(Expense Tracker)</h3>
+          
           <h4>MERN </h4>
+          <a href="https://github.com/acpm1221/money_mate_repo" target="_blank">View on GitHub</a><br></br>
+          <a href="https://money-mate-repo.vercel.app/" target="_blank">Hosted Link</a>
+          <br></br>
+          <p>
+            A full-stack web application that allows users to track their income
+            and expenses in real-time. Built with React for the frontend,
+            Node.js and Express for the backend, and MongoDB for data storage.
+            Users can log in, add or delete transactions, view financial
+            summaries, and manage their personal finances securely and
+            efficiently.
+          </p>
+        </div>
+        <div className="project-item">
+          <h3>Food Delivery</h3>
+          
+          <h4>MERN </h4>
+          <a href="https://github.com/acpm1221/food-del" target="_blank">View on GitHub</a><br></br>
+          <a href="https://github.com/acpm1221/food-del" target="_blank">Hosted Link</a>
           <br></br>
           <p>
             A full-stack web application that allows users to track their income
@@ -23,6 +42,8 @@ const Projects = ({ id }) => {
         <div className="project-item">
           <h3>Personal portfolio</h3>
           <h4>React</h4>
+          <a href="https://github.com/acpm1221/portfolio" target="_blank">View on GitHub</a><br></br>
+          
           <br></br>
           <p>
             A modern, responsive portfolio website built with React to showcase
@@ -31,21 +52,12 @@ const Projects = ({ id }) => {
             highlight development experience.
           </p>
         </div>
-        <div className="project-item">
-          <h3>GeeksFood</h3>
-          <h4>MERN</h4>
-          <br></br>
-          <p>
-            A full-stack food ordering platform built using the MERN stack
-            (MongoDB, Express.js, React, Node.js). Users can browse restaurants,
-            view menus, place orders, and track delivery status in real-time.
-            Includes user authentication, order management, and a responsive,
-            intuitive interface.
-          </p>
-        </div>
+        
         <div className="project-item">
           <h3>Youtube homepage clone</h3>
           <h4>HTML CSS JavaScript</h4>
+          <a href="https://github.com/acpm1221/youtube-clone" target="_blank">View on GitHub</a><br></br>
+          <a href="https://stately-halva-d1f837.netlify.app/" target="_blank">Hosted Link</a>
           <br></br>
           <p>
             A static clone of the YouTube homepage built using HTML and CSS,
@@ -56,6 +68,8 @@ const Projects = ({ id }) => {
         <div className="project-item">
           <h3>Emoji search app</h3>
           <h4>HTML CSS JavaScript</h4>
+          <a href="https://github.com/acpm1221/pokemon" target="_blank">View on GitHub</a><br></br>
+          <a href="https://acpm1221.github.io/pokemon/" target="_blank">Hosted Link</a>
           <br></br>
           <p>
             A simple and fast emoji search tool built with Vanilla JavaScript
@@ -66,6 +80,8 @@ const Projects = ({ id }) => {
         <div className="project-item">
           <h3>Weather App</h3>
           <h4>HTML CSS JavaScript</h4>
+          <a href="https://github.com/acpm1221/weather" target="_blank">View on GitHub</a><br></br>
+          <a href="https://gregarious-taffy-b06274.netlify.app/" target="_blank">Hosted Link</a>
           <br></br>
           <p>
             A dynamic weather application built with Vanilla JavaScript that
